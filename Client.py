7fff2896c13b53f5c93f9c4120934195605ecf19
Benjamin Girard <benjamin.girard.protic@gmail.com>
2020-02-17 11:28:16 -0500

    Added TCP connection to get the players' actions

--- Client.py
+++ Client.py
@@ -1,19 +1,40 @@
 import socket
+import threading
+import time
 
 
 class Client:
     def __init__(self):
         self.game_state = {}
+        self.game_state_port = 12000
         self.game_state_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.game_state_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         self.game_state_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
+        self.action_port = 50000
+        self.action_socket = socket.socket()
+        self.action_host = socket.gethostname()
+
     def start(self):
-        self.game_state_socket.bind(("", 12000))
+        t1 = threading.Thread(target=self.start_game_updates_socket)
+        t1.start()
+
+        t2 = threading.Thread(target=self.start_actions_socket)
+        t2.start()
+
+    def start_game_updates_socket(self):
+        self.game_state_socket.bind(("", self.game_state_port))
         while True:
             data, addr = self.game_state_socket.recvfrom(1024)
             self.game_state = self.decode_game_state(data)
             print(data)
+
+    def start_actions_socket(self):
+        self.action_socket.connect((self.action_host, self.action_port))
+        while True:
+            msg = b"actions"
+            self.action_socket.send(msg)
+            time.sleep(1)
 
     @staticmethod
     def decode_game_state(data):
--- Server.py
+++ Server.py
@@ -2,13 +2,14 @@ from collections import deque
 import socket
 import _thread
 import time
+import threading
 
 
 class Server:
     def __init__(self, Game):
         self.Game = Game
 
-        self.action_port = 5000
+        self.action_port = 50000
         self.action_socket = socket.socket()
         self.action_host = socket.gethostname()
 
@@ -26,14 +27,24 @@ class Server:
     def on_new_client(client_socket, addr):
         #TODO: break out de ça
         while True:
-            msg = client_socket.recv(1024)
+            data = client_socket.recv(1024)
+            actions = Server.decode_actions(data)
+            print(actions)
+            # TODO: do something with actions
 
-            msg = ""
-            client_socket.send(msg)
         clientsocket.close()
 
+    @staticmethod
+    def decode_actions(data):
+        actions = data
+        return actions
+
     def start(self):
-        pass
+        t1 = threading.Thread(target=self.serve_game_state_updates)
+        t1.start()
+
+        t2 = threading.Thread(target=self.serve_actions)
+        t2.start()
 
     def __start(self):
         while self.server_on:
@@ -45,7 +56,7 @@ class Server:
         self.queue.appendleft(action)
 
     def serve_actions(self):
-        self.action_socket.bind((self.host, self.port))
+        self.action_socket.bind((self.action_host, self.action_port))
         self.action_socket.listen(5)
 
         #TODO: break out de ça
@@ -56,11 +67,11 @@ class Server:
 
     def serve_game_state_updates(self):
         while True:
-            message = b"hello\n"
+            message = b"game update\n"
             self.game_state_socket.sendto(message, (self.game_state_host, self.game_state_port))
             time.sleep(0.5)
 
 
 if __name__ == '__main__':
     server = Server('')
-    server.serve_game_state_updates()
+    server.start()
