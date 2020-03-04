3f8fe5a1eadbe813a921f5a2d2897afbeaa4ee02
Benjamin Girard <benjamin.girard.protic@gmail.com>
2020-03-03 23:23:17 -0500

    Initial Room and Map Design

--- src/game/map.py
+++ src/game/map.py
@@ -1,3 +1,47 @@
+import random
+
+from src.game.room import Room
+
+
 class Map:
-    def __init__(self):
-        pass
+    def __init__(self, players, rooms_per_player=3, room_connections=2):
+        self.rooms_per_player = rooms_per_player
+        self.nb_generate_room_connections = room_connections
+        self.rooms = self.generate_rooms(players)
+        self.connect_rooms()
+
+    # Ugly code
+    # Add tests
+    def generate_rooms(self, players):
+        nb_players = len(players)
+        nb_starting_area_rooms = nb_players
+        nb_other_rooms = self.rooms_per_player * nb_players - nb_players
+        rooms = []
+
+        for i in range(nb_starting_area_rooms):
+            room_difficulty = 'starting_area'
+            room = Room(room_difficulty)
+            room.add_player(players[i])
+            rooms.append(room)
+
+        for i in range(nb_other_rooms):
+            room_difficulty = random.choice(['medium_area', 'hard_area'])
+            rooms.append(Room(room_difficulty))
+
+        return rooms
+
+    # Duplicate code
+    # Add tests
+    def connect_rooms(self):
+        other_rooms = list(filter(lambda room: room.type != 'starting_area', self.rooms))
+        for room in self.rooms:
+            if room.type == 'starting_area':
+                connections = random.sample(other_rooms, self.nb_generate_room_connections)
+                for other_room in connections:
+                    room.add_neighboring_room(other_room)
+
+            else:
+                connections = random.sample(self.rooms, self.nb_generate_room_connections)
+                for other_room in connections:
+                    room.add_neighboring_room(other_room)
+
--- src/game/room.py
+++ src/game/room.py
@@ -1,3 +1,47 @@
+import random
+
+
 class Room:
-    def __init__(self):
-        pass
+    def __init__(self, room_difficulty):
+        self.type = type
+        self.click_bonus, self.income_bonus, self.room_size = self.get_room_data_from_difficulty(room_difficulty)
+
+        self.players = set()
+        self.neighboring_rooms = set()
+        self.discovered = False
+
+    # Add test
+    def add_player(self, player):
+        self.players.add(player)
+
+    # Add test
+    def remove_player(self, player):
+        self.players.remove(player)
+
+    # Add test
+    def add_neighboring_room(self, other_room):
+        self.neighboring_rooms.add(other_room)
+
+    # Add multiple tests
+    @staticmethod
+    def get_room_data_from_difficulty(room_difficulty):
+        if room_difficulty == 'starting_area':
+            click_bonus = 1
+            income_bonus = 1
+            room_size = 5
+
+        elif room_difficulty == 'medium_area':
+            click_bonus = 1
+            income_bonus = 1
+            room_size = 5
+
+        elif room_difficulty == 'hard_area':
+            click_bonus = 1
+            income_bonus = 1
+            room_size = 5
+
+        else:
+            raise Exception("Invalid Room Type")
+
+        return click_bonus, income_bonus, room_size
+
